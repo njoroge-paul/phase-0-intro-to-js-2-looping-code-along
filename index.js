c163afcab1697c19154f3e82e8af5cc2be38cdb6
Duncan <njorogedun870@gmail.com>
2024-07-18 09:06:39 +0300

    finished the test

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solutions in this file
+// index.js
+
+function writeCards(names, giftType) {
+    return names.map(name => `Thank you, ${name}, for the wonderful ${giftType} gift!`);
+  }
+  
+  function countDown(n) {
+    for (let i = n; i >= 0; i--) {
+      console.log(i);
+    }
+  }
+  
+  module.exports = { writeCards, countDown };
+
+ 
